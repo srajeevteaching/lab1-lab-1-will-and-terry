75711c6aacdfafea0ade75566bf3cc81fc561d7f
Will <wpgrimmer@loyola.edu>
2021-09-16 09:52:51 -0400

    finished lab 1

---
+++ conversion.py
@@ -0,0 +1,6 @@
+mil = float(input("Please input a number of milliliters"))
+teaspoons=(mil/5)
+tablespoons=(mil/15)
+
+print("there are " + str(teaspoons) + " teaspoons in " + str(mil) + " milliliters")
+print("there are " + str(tablespoons) + " tablespoons in " + str(mil) + " milliliters")
